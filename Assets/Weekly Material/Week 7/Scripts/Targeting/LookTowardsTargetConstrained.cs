5e96fa0bccc25fb24d117d5a6cfdc70e8d84a15b
Aeon Amberwood <aeon.amberwood@icloud.com>
2026-09-04 13:36:57 +1000

    Refined Wk 7

--- Assets/Weekly Material/Week 7/Scripts/Targeting/LookTowardsTargetConstrained.cs
+++ Assets/Weekly Material/Week 7/Scripts/Targeting/LookTowardsTargetConstrained.cs
@@ -6,6 +6,9 @@ public class LookTowardsTargetConstrained : LookTowardsTarget
     [Tooltip("The maximum angle to rotate away from the initial direction")]
     public float angleConstraint = 45;
 
+    [Range(0, 1)]
+    public float verticalConstraint = .5f;
+
     // Hold the direction this object started out at
     protected Vector3 directionInitial;
 
@@ -17,10 +20,18 @@ public class LookTowardsTargetConstrained : LookTowardsTarget
 
     public override void LookTowards(Vector3 direction, float speed)
     {
+        Vector3 relativeInitial = directionInitial;
+
+        // If we have a parent transform, we should use it's direction as a reference
+        if (transform.parent)
+        {
+            relativeInitial = transform.parent.TransformDirection(directionInitial);
+        }
+
         // By first rotating from our initial direction towards our target direction,
         // limiting by our maximum rotation angle,
         // we determine the direction we're allowed to rotate towards
-        Vector3 directionPossible = Vector3.RotateTowards(directionInitial, direction, Mathf.Deg2Rad * angleConstraint, 0);
+        Vector3 directionPossible = Vector3.RotateTowards(relativeInitial, direction, Mathf.Deg2Rad * angleConstraint, 0);
 
         // Now we can use this new direction with the default behaviour
         base.LookTowards(directionPossible, speed);
